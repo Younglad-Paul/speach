12a471745b6ffbdaf53deb3cbec5e638a21bc707
Younglad-Paul <borisadedayopaul@gmail.com>
2025-09-25 15:18:51 +0100

    Add HTTPS redirect to fix speech recognition

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://speach-nine.vercel.app/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
